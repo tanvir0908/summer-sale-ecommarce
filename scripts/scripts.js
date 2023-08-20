e5932a5d95ab679c8ad04e563b44f744607882b2
Tanvir Hasan Emon <tanvirhasan.th157@gmail.com>
2023-08-20 13:42:04 +0600

    Completed
    Completed project version 1.0

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -9,6 +9,7 @@ function totalProductPrice(element) {
   purchaseButtonEnable(newTotalPrice);
 }
 
+// Button Conditions
 function purchaseButtonEnable(price) {
   if (price > 0 && price >= 200) {
     document.getElementById("purchase-button").disabled = false;
@@ -22,24 +23,42 @@ function purchaseButtonEnable(price) {
   }
 }
 
+// add to cart list
+function addToCartList(productName) {
+  const cartList = document.getElementById("cart-list");
+  const count = cartList.childElementCount;
+  const p = document.createElement("p");
+  p.innerHTML = `
+    ${count + 1}.
+    ${productName}
+  `;
+  cartList.appendChild(p);
+}
+
 // Cart Click
 document.getElementById("black-cap").addEventListener("click", function () {
   totalProductPrice("black-cap-price");
+  addToCartList("Sports Black Cap");
 });
 document.getElementById("jersey-set").addEventListener("click", function () {
   totalProductPrice("jersey-set-price");
+  addToCartList("Full Jersey Set");
 });
 document.getElementById("sports-cates").addEventListener("click", function () {
   totalProductPrice("sports-cates-price");
+  addToCartList("Sports cates");
 });
 document.getElementById("relax-chair").addEventListener("click", function () {
   totalProductPrice("relax-chair-price");
+  addToCartList("Single Relax Chair");
 });
 document.getElementById("chidren-chair").addEventListener("click", function () {
   totalProductPrice("chidren-chair-price");
+  addToCartList("Children play");
 });
 document.getElementById("sofa-chair").addEventListener("click", function () {
   totalProductPrice("sofa-chair-price");
+  addToCartList("Flexible Sofa");
 });
 
 //Coupon Button Click
